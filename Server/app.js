1f179f999ace2d6713dabc5f9ce237d688ac3b54
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-03-18 08:02:00 +0200

    Backend server and extension

--- Server/app.js
+++ Server/app.js
@@ -1,34 +1,43 @@
 var express = require('express')
+const path = require('path')
 var app = express()
 var ejs = require('ejs');
 var bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
-
 app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () =>
     console.log('Example app listening on port 3000!'));
 
 app.get('/', function (req, res) {
-    res.render("homepage")
+    res.render("index")
 });
 
+app.get('/summarize', function (req, res) {
+    res.render("summarize")
+})
+
 app.post('/upload', function (req, res) {
-    text = req.params['text']
+    text = req.body.text
+    ext = req.body.sender
+    console.log(text)
     //fs.appendFileSync(path.join(__dirname, '../datasets/liver.csv'), "\n" + result.join(','));
     var spawn = require("child_process").spawn;
-    var process = spawn('python3', [path.join(__dirname, "../models/modelLoader.py"), "model"].concat(text));
+    var process = spawn('C:\\ProgramData\\Anaconda3\\python.exe', [path.join(__dirname, "../models/modelLoader.py"), "model", text]);
 
     process.stdout.on('data', function (data) {
         console.log(data.toString())
-        res.render('result', {
-            text: result
-        });
+        if(ext == "ext") {
+            res.send(data);
+        }else {
+            res.render('result', {
+                text: data
+           });
+        }
     });
 
     process.stderr.on('data', function (data) {
@@ -36,9 +45,4 @@ app.post('/upload', function (req, res) {
     });
 
 
-});
-
-app.post("/sum", function (req, res) {
-    console.log(req.body.text);
-    res.send(req.body.text)
 });
